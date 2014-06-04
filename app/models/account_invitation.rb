0ead7f9a2fff3e3d1e32d7dc3cba3761f2f3f88b
Brian Fitch <brianmfitch@gmail.com>
2014-06-04 10:47:21 -0400

    Add uniqueness validation on email to prevent multiple invites. Make login emails unique too.

--- app/models/account_invitation.rb
+++ app/models/account_invitation.rb
@@ -1,8 +1,3 @@
 class AccountInvitation < ActiveRecord::Base
-  #TODO: add completed column, set to true when link is clicked
-  #lookup invitation where `completed` is false to prevent 
-  #reusing link and
-  #
-  #check for invite_code at beginning of action. If doesn't match
-  #return unauthorized
+  validates :invitee_email, uniqueness: { message: "User has already been invited" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :email, uniqueness: true
 end
